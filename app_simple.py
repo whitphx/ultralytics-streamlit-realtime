a63dce4ff131eda56bf5ae441cb363140947431d
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2025-08-06 15:51:37 +0900

    Show info

--- app_simple.py
+++ app_simple.py
@@ -1,38 +1,20 @@
 import streamlit as st
-from queue import Queue
 from streamlit_webrtc import webrtc_streamer, WebRtcMode
 from ultralytics import YOLO
 
 with st.spinner("Loading YOLO model..."):
-    model = YOLO("yolo11n.pt")
-
-info_queue = Queue()
+    model = YOLO("yolov8n.pt")
 
 def video_frame_callback(input_frame):
     img = input_frame.to_ndarray(format="bgr24")
     results = model(img)
     annotated_frame = results[0].plot()
-    info_queue.put({
-        "speed": results[0].speed,
-        "summary": results[0].summary()
-    })
     output_frame = input_frame.from_ndarray(annotated_frame, format="bgr24")
     return output_frame
 
-ctx = webrtc_streamer(
+webrtc_streamer(
     key="yolo",
     mode=WebRtcMode.SENDRECV,
     video_frame_callback=video_frame_callback,
     media_stream_constraints={"video": True, "audio": False}
 )
-
-info_area = st.empty()
-
-while ctx.state.playing:
-    info = info_queue.get()
-
-    with info_area.container():
-        st.subheader("Speed")
-        st.write(info["speed"])
-        st.subheader("Summary")
-        st.write(info["summary"])
